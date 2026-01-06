000606d3e10539d2e73dfbe31442f2a7006b0c67
dylee <ldyeon20@naver.com>
2026-01-06 15:58:34 +0900

    bookbook_pjt

--- book_community_practice/backend/books/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
-
-router = DefaultRouter()
-router.register(r'books', BookViewSet)  # /books 엔드포인트 등록
-
-urlpatterns = router.urls
-
--- book_community_practice/backend/config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from books.urls import router
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),  # 모든 books API는 /api/books로 접근
-]
